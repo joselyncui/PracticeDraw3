0325ab1b83cc4b8c5d4e25d4a2ef9c69c2b5048d
yao.cui <yao.cui@weimob.com>
2018-01-05 16:57:17 +0800

    作业提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -44,12 +44,15 @@ public class Practice14GetFontMetricsView extends View {
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
 
+        Paint.FontMetrics fm = new Paint.FontMetrics();
+        paint2.getFontMetrics(fm);
+
         int middle = (top + bottom) / 2;
-        canvas.drawText(texts[0], 100, middle, paint2);
-        canvas.drawText(texts[1], 200, middle, paint2);
-        canvas.drawText(texts[2], 300, middle, paint2);
-        canvas.drawText(texts[3], 400, middle, paint2);
-        canvas.drawText(texts[4], 500, middle, paint2);
-        canvas.drawText(texts[5], 600, middle, paint2);
+        canvas.drawText(texts[0], 100, middle-(fm.ascent+fm.descent)/2, paint2);
+        canvas.drawText(texts[1], 200, middle-(fm.ascent+fm.descent)/2, paint2);
+        canvas.drawText(texts[2], 300, middle-(fm.ascent+fm.descent)/2, paint2);
+        canvas.drawText(texts[3], 400, middle-(fm.ascent+fm.descent)/2, paint2);
+        canvas.drawText(texts[4], 500, middle-(fm.ascent+fm.descent)/2, paint2);
+        canvas.drawText(texts[5], 600, middle-(fm.ascent+fm.descent)/2, paint2);
     }
 }
